2494b1f175cdc6e8dafbbc15c348998f69982d82
spiritphyz <tonyle@ocf.berkeley.edu>
2016-11-18 11:18:08 -0800

    Modularlize the 'getWiki' route

--- server/server.js
+++ server/server.js
@@ -1,13 +1,10 @@
-const bodyParser = require('body-parser');
-const request = require('request');
 const metadata = require('../package.json');
-const Entities = require('html-entities').AllHtmlEntities;
 const express = require('express');
 const app = express();
 const path = require('path');
+const utils = require('./lib/utilities.js');
 const port = 3000;
 
-const entities = new Entities(); // decode strings like '&amp;'
 const routes = ['/', '/sf', '/lobby', '/louvre', '/berlin', '/milan/', '/rome'];
 
 
@@ -25,30 +22,10 @@ routes.forEach(function(route) {
 //
 //   https://en.wikipedia.org/wiki/Macy's
 //   https://en.wikipedia.org/wiki/Normandy_landings
-
+//   https://en.wikipedia.org/wiki/Sant%27Agnese_in_Agone
 
 app.get('/getWiki', function(req, res) {
-  const url = 'http://en.wikipedia.org/w/api.php?action=query&prop=extracts&titles='
-              + req.query.exactWikiTitle 
-              + '&format=json&exintro=1';
-  request(url, (err, requestResponse, body) => {
-    if (err) {
-      console.log('Error in Wikipedia fetch', err);
-    } else {
-      const query = (JSON.parse(body)).query.pages;
-      const text = query[(Object.keys(query)[0])].extract;
-      const regex = /(<([^>]+)>)/ig;
-      const firstParagraph = text.slice(0, text.indexOf('\n'));
-      const result = firstParagraph.replace(regex, '');
-      
-      const regexApostrophes = /(\')/ig;
-      let output = result.replace(regexApostrophes, '\'');
-      output = entities.decode(output);
-      console.log('Sending scrubbed wiki text:', output);
-
-      res.status(200).send(JSON.stringify(output));
-    }
-  });
+  utils.fetchWiki(req, res);
 });
 
 
